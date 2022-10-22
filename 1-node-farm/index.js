b0d723fac1e8fc78fae753c894ec63510dc7a475
Daniela Barbosa <contato@danielacb.com>
2022-10-21 23:38:02 -0300

    node-farm: use slugify on product page url

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,10 +1,16 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+dataObj.reduce((acc, cur) => {
+  cur["id"] = slugify(cur.productName, { lower: true });
+  return cur;
+}, []);
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -38,7 +44,7 @@ const server = http.createServer((req, res) => {
   } else if (pathname === "/product") {
     res.writeHead(200, { "Content-type": "text/html" });
 
-    const product = dataObj[query.id];
+    const product = dataObj[dataObj.map((e) => e.id).indexOf(query.id)];
     const output = replaceTemplate(templateProduct, product);
 
     res.end(output);
