0e726e3373b2e34eb77822fd86b2aa396ed41d49
Daniela C. Barbosa <contato@danielacb.com>
2023-02-17 20:48:10 -0300

    natours: setup environment variables

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   console.log('Here is the middleware');
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+// console.log(app.get('env'));
+// console.log(process.env);
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`🚀 App running on port ${port}...`);
 });
